ccce5807f01c0074d62c23378a6aceffffc425c5
tonyjackso <97927100+tonyjackso@users.noreply.github.com>
2022-01-19 09:21:50 +0700

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -13,10 +13,10 @@ async function getName() {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (500000 daily)'));
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+    if (tokens > 500000) {
+        alert('You can only add up to 500000 tokens daily.');
     };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -28,7 +28,7 @@ async function addCurrencies() {
         credentials: "include",
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 30000,
             name: await getName()
         })
     });
